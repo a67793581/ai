84c9e9dae04af5b890c05510df8c4d16318ad61b
VicciWang <wwang002@gold.ac.uk>
2024-03-30 15:45:37 +0800

    audiosource

--- Nova330/Assets/Emojis/Anim/TestAnim.cs
+++ Nova330/Assets/Emojis/Anim/TestAnim.cs
@@ -4,22 +4,36 @@ using UnityEngine;
 
 public class TestAnim : MonoBehaviour
 {
+    public AudioClip[] ads;
     public int index;
     public Animator aniL;
     public Animator aniR;
 
     public string emotionState = "EmotionState";
-
+    public AudioSource ad;
+    public bool playSwitch;
 
     // Update is called once per frame
     void Update()
     {
         SetAnim(aniL, aniR);
+        SetAudio();
     }
 
     void SetAnim(Animator l, Animator r)
     {
         l.SetInteger(emotionState, index);
         r.SetInteger(emotionState, index);
+    }
+
+    void SetAudio()
+    {
+        if (playSwitch)
+            return;
+        
+        ad.clip = ads[index];
+        
+        ad.Play();
+        playSwitch = !playSwitch;
     }
 }
